72fa5ae6201353f2181df75321edc2950753f0da
oreo21 <reok99@gmail.com>
2017-01-09 15:33:41 -0500

    testing login process; my brain can't handle anything else -- kaboom!

---
+++ csvTest.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(){
+  authenticate();
+  return 0;
+}
+
+void authenticate(){
+  int status;
+  if () register;
+  else status == 0;
+}
+
+int userExists(user){
+  return 0; 
+}
+
+void registration(user, password){
+}
+
+void login(user, password){
+}
+
+
+
+  printf("Enter username: ");
+  fgets(name, sizeof(name), stdin);
+  *(strchr(input, '\n'));
+}
